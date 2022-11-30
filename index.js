e70bbd656e778b2d74031e03a8269091b3d84fd4
Joshua Duffney <jduffney@microsoft.com>
2022-11-30 18:00:08 +0000

    add notationcli version check logic

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ async function setup() {
     const version = core.getInput('version'); //"0.9.0-alpha.1";
     const keyName = core.getInput('key_name');
 
+    // Check if the NotationCli version is supported
+    versionCheck(version);
+
     // Download the specific version of the tool, e.g. as a tarball/zipball
     const download = getDownloadObject(version);
     console.log(download)
@@ -36,8 +39,8 @@ async function setup() {
     core.addPath(path.join(pathToCLI, download.binPath));
 
     // Install Notation plugin
-    const pluginName = core.getInput('plugin_name'); // "notation-azure-kv";
-    const pluginVersion = core.getInput('plugin_version'); //"0.3.1-alpha.1";
+    const pluginName = core.getInput('plugin_name');
+    const pluginVersion = core.getInput('plugin_version');
 
     if (pluginName) {
       setupPlugin(pluginName, pluginVersion, keyName);
