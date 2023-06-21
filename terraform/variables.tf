0a56b76dccbfc41eec527afce1547ef0967f1157
Joshua Duffney <jduffney@microsoft.com>
2023-06-21 15:21:14 -0500

    Test: terraform deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "acr_name" {
+  type = string
+}
+
+variable "kv_name" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+  default = "eastus"
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "cert_name" {
+  type = string
+}
